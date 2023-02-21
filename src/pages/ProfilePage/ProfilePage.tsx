afcd36d3acd8b719b56eebacd62f5a72536cf23e
Serhii Demydenko <demydenko.serhii1994@gmail.com>
2023-02-21 16:36:48 +0200

    fix: gh pages and some fix

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -14,12 +14,21 @@ const ProfilePage = () => {
         sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
       >
         <Grid item xs={12} sm={4}>
-          <Avatar src='https://picsum.photos/200' />
+          <Avatar
+            sx={{
+              width: '150px',
+              height: '150px',
+              marginRight: '20px',
+            }}
+            src='https://picsum.photos/200'
+          />
         </Grid>
         <Grid item xs={12} sm={8}>
-          <Paper>
+          <Paper sx={{ padding: '20px' }}>
             <Typography variant='h4'>John Doe</Typography>
-            <Typography variant='subtitle1'>Email: john.doe@example.com</Typography>
+            <Typography variant='subtitle1'>
+              Email: john.doe@example.com
+            </Typography>
             <Typography variant='subtitle1'>Phone: 123-456-7890</Typography>
             <Button variant='contained' color='primary'>
               Edit Profile
