e62290f11e7b791eb25e7df95b9e829318f59237
Serhii Demydenko <demydenko.serhii1994@gmail.com>
2023-02-21 12:30:35 +0200

    feat: update header

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,110 +2,62 @@ import { Container } from '@mui/material'
 import * as React from 'react'
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
-import CssBaseline from '@mui/material/CssBaseline'
-import Divider from '@mui/material/Divider'
-import Drawer from '@mui/material/Drawer'
+
 import IconButton from '@mui/material/IconButton'
-import List from '@mui/material/List'
-import ListItem from '@mui/material/ListItem'
+
 import MenuIcon from '@mui/icons-material/Menu'
 import Toolbar from '@mui/material/Toolbar'
-import { Link, NavLink } from 'react-router-dom'
-import NewspaperIcon from '@mui/icons-material/Newspaper'
-interface Props {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  window?: () => Window
+import { NavLink } from 'react-router-dom'
+import UserMenu from '../Header/UserMenu'
+import Logo from '../Logo'
+
+interface Item {
+  id: number
+  name: string
+  path: string
 }
-
-const drawerWidth = 240
-const navItems = [
-  {
-    id: 1,
-    name: 'Home',
-    path: '/',
-  },
-  {
-    id: 2,
-    name: 'News',
-    path: '/news',
-  },
-]
-
-const NavBar = (props: Props) => {
-  const { window } = props
-  const [mobileOpen, setMobileOpen] = React.useState(false)
-
-  const handleDrawerToggle = () => {
-    setMobileOpen((prevState) => !prevState)
-  }
-
-  const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-      <Link to='/'>
-        <NewspaperIcon />
-      </Link>
-      <Divider />
-      <List>
-        {navItems.map((item) => (
-          <ListItem key={item.id} disablePadding>
-            <NavLink to={item.path}>{item.name}</NavLink>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  )
-
-  const container = window !== undefined ? () => window().document.body : undefined
-
+interface INavBar {
+  handleDrawerToggle: () => void
+  navItems: Item[]
+}
+const NavBar: React.FC<INavBar> = ({ handleDrawerToggle, navItems }) => {
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar component='nav'>
-        <Toolbar>
-          <Container sx={{ display: 'flex' }}>
-            <IconButton
-              color='inherit'
-              aria-label='open drawer'
-              edge='start'
-              onClick={handleDrawerToggle}
-              sx={{ mr: 2, display: { sm: 'none' } }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Link to='/'>
-              <NewspaperIcon />
-            </Link>
-            <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-              {navItems.map((item) => (
-                <NavLink to={item.path} key={item.id}>
-                  {item.name}
-                </NavLink>
-              ))}
-            </Box>
-          </Container>
-        </Toolbar>
-      </AppBar>
-      <Box component='nav'>
-        <Drawer
-          container={container}
-          variant='temporary'
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-        >
-          {drawer}
-        </Drawer>
-      </Box>
-    </Box>
+    <AppBar component='nav'>
+      <Toolbar sx={{ bgcolor: '#272343' }}>
+        <Container sx={{ display: 'flex', alignItems: 'center' }}>
+          <IconButton
+            color='inherit'
+            aria-label='open drawer'
+            edge='start'
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Logo style={{ marginRight: '40px' }} />
+          <Box
+            sx={{
+              display: { xs: 'none', sm: 'flex' },
+              gap: { sm: '40px' },
+            }}
+          >
+            {navItems.map((item) => (
+              <NavLink
+                to={item.path}
+                key={item.id}
+                style={{
+                  color: 'white',
+                  fontWeight: '500',
+                }}
+              >
+                {item.name}
+              </NavLink>
+            ))}
+          </Box>
+          <UserMenu />
+        </Container>
+      </Toolbar>
+    </AppBar>
   )
 }
 
